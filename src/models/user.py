ab489b47bb2504055ab0eb8212ab4bcd3fabe882
cvalle <cuitlahuac92@hotmail.com>
2020-03-22 17:31:02 -0600

    Updated user's method

--- src/models/user.py
+++ src/models/user.py
@@ -1,21 +1,71 @@
 __author__ = 'CJVR'
 
+import uuid
+
+from flask import session
+
+from src.common.database import Database
+
+
 class User:
-    def __init__(self):
-        pass
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one('users', {'email': email})
+        if data is not None:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one('users', {'_id': _id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
+    @staticmethod
+    def login_valid(email, password):
+        # User.login_valid('someemail@email.com', 'somepassword')
         # Check whether a user's email matches the password they sent us
-        pass
+        user = User.get_by_email(email)
+        if user is not None:
+            # Check the password
+            return user.password == password
+        return False
 
-    def register(self):
-        pass
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        if user is not None:
+            # User does not exist
+            new_user = cls(email, password)
+            new_user._save_to_mongo()
+            session['email'] = email
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def login(user_email):
+        # login_valid has already been called
+        session['email'] = user_email
+
+    @staticmethod
+    def logout(user_email):
+        session['email'] = None
+
 
     def get_blogs(self):
         pass
+
+    def json(self):
+        return {
+            'email': self.email,
+            '_id': self._id,
+            'password': self.password
+        }
+
+    def _save_to_mongo(self):
+        Database.insert('users', self.json())
