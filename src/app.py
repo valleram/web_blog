c5b34cda0701fe65826b25babee479493b7a2f5b
cvalle <cuitlahuac92@hotmail.com>
2020-03-28 11:32:35 -0600

    render templates added

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 __author__ = 'CJVR'
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # '__main__'
 
 
 @app.route('/')
 def hello_method():
-    return "Hello, world!"
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
